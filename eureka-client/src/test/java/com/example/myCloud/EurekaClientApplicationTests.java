0a57571f7cb65dc3ff02eb1de9612ee6976a16c2
jiale.yan <jiale.yan@ximalaya.com>
2023-06-30 16:28:05 +0800

    eureka-server eureka-client

---
+++ eureka-client/src/test/java/com/example/myCloud/EurekaClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myCloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
